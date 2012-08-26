f6bf28cb295433acfe2870c44313e653868935e4
Tom K <krajeec@gmail.com>
2012-08-27 00:44:41 +0200

    implement downloading and displaying of photos at selected place

--- FlickerPictures/DetailPlaceViewController.m
+++ FlickerPictures/DetailPlaceViewController.m
@@ -9,13 +9,24 @@
 #import "DetailPlaceViewController.h"
 #import "FlickrFetcher.h"
 
+@interface DetailPlaceViewController()
+@property (strong, nonatomic) NSArray *photos;
+@end
+
 @implementation DetailPlaceViewController
 @synthesize locationInfo = _locationInfo;
+@synthesize photos = _photos;
 
 - (void)setLocationInfo:(NSDictionary *)locationInfo
 {
     _locationInfo = locationInfo;
     self.title = [locationInfo objectForKey:FLICKR_PLACE_NAME];
+}
+
+- (void)setPhotos:(NSArray *)photos
+{
+    _photos = photos;
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -33,6 +44,10 @@
     [super viewWillAppear:animated];
     
     // download photos from selected location
+    dispatch_queue_t downloadQueue = dispatch_queue_create("photos at place downloader", nil);
+    dispatch_async(downloadQueue, ^{
+        self.photos = [FlickrFetcher photosInPlace:self.locationInfo maxResults:50];
+    });
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -56,25 +71,16 @@
 	return YES;
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
+#pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return self.photos.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"Photo Table Cell";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
@@ -82,6 +88,8 @@
     }
     
     // Configure the cell...
+    cell.textLabel.text = [[self.photos objectAtIndex:indexPath.row] objectForKey:FLICKR_PHOTO_TITLE];
+    cell.detailTextLabel.text = [[self.photos objectAtIndex:indexPath.row] objectForKey:FLICKR_PHOTO_DESCRIPTION];
     
     return cell;
 }
