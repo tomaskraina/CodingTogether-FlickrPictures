15121e70840d5fc35122ca4c113490b35473e645
Tom K <krajeec@gmail.com>
2012-08-26 22:52:15 +0200

    change the name of view controllers

---
+++ FlickerPictures/RecentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecentsViewController.h
+//  FlickerPictures
+//
+//  Created by Tom Kraina on 26.08.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecentsViewController : UIViewController
+
+@end
