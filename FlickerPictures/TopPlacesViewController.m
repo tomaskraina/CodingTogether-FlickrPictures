3998feb7a5d7c8fbe4fb24f98c798fb4c6e8cef1
Tom K <krajeec@gmail.com>
2012-08-26 23:36:05 +0200

    implement downloading and displaying top places

--- FlickerPictures/TopPlacesViewController.m
+++ FlickerPictures/TopPlacesViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "TopPlacesViewController.h"
+#import "FlickrFetcher.h"
+
+@interface TopPlacesViewController() <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) NSArray *places;
+@end
 
 @implementation TopPlacesViewController
+@synthesize places = _places;
+
+- (void)setPlaces:(NSArray *)places
+{
+    _places = places;
+    [self.tableView reloadData];
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -34,6 +46,12 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    
+    // download the list of photos from flicker in another thread
+    dispatch_queue_t downloadQueue = dispatch_queue_create("top places downloader", NULL);
+    dispatch_async(downloadQueue, ^{
+        self.places = [FlickrFetcher topPlaces];
+    });
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -56,5 +74,30 @@
     // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.places.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Top Place Table Cell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    NSString *place = [[self.places objectAtIndex:indexPath.row] objectForKey:FLICKR_PLACE_NAME];
+    NSUInteger positionOfFirstComma = [place rangeOfString:@","].location;
+    cell.textLabel.text = [place substringToIndex:positionOfFirstComma];
+    cell.detailTextLabel.text = [place substringFromIndex:positionOfFirstComma+2];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
 
 @end
