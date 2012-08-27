7d8bfeb1e28a38a8fba7e48e7581a47fa18b0d88
Tom K <krajeec@gmail.com>
2012-08-27 20:05:13 +0200

    implement caching of photos

--- FlickrPictures/SinglePhotoViewController.m
+++ FlickrPictures/SinglePhotoViewController.m
@@ -64,12 +64,92 @@
     return self.photoInfo.description;
 }
 
+#pragma mark - Cache
+
+- (NSURL *)photosCacheDirURL
+{
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSURL *cacheDirURL = [[fileManager URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] lastObject];
+    NSURL *photosCacheDirURL = [cacheDirURL URLByAppendingPathComponent:@"photos" isDirectory:YES];
+    
+    if (![fileManager fileExistsAtPath:photosCacheDirURL.path]) {
+        [fileManager createDirectoryAtURL:photosCacheDirURL withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+    
+    return photosCacheDirURL;
+}
+
+- (NSUInteger)sizeOfDirectoryURL:(NSURL *)url
+{
+    NSUInteger filesize = 0;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *directoryString = url.path;
+    NSArray *filenames = [fileManager contentsOfDirectoryAtPath:directoryString error:nil];
+    for (NSString *filename in filenames) {
+        NSError *error;
+        NSDictionary *fileAttributes = [fileManager attributesOfItemAtPath:[directoryString stringByAppendingPathComponent:filename] error:&error];
+        filesize += [fileAttributes fileSize];
+    }
+    
+    return filesize;
+}
+
+- (void)cleanUpCacheToFitMaxSize:(NSUInteger)megabytes
+{
+    if ([self sizeOfDirectoryURL:[self photosCacheDirURL]] < megabytes*1024*1024) {
+        return;
+    }
+    
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSString *directoryString = [self photosCacheDirURL].path;
+    NSArray *filenames = [fileManager contentsOfDirectoryAtPath:directoryString error:nil];
+
+    // remove the oldest files
+    // sorted by created datetime desc (oldest first)
+    NSArray *filenamesByCreation = [filenames sortedArrayUsingComparator: ^(id a, id b){
+        NSDictionary *fileAttributes1 = [fileManager attributesOfItemAtPath:[directoryString stringByAppendingPathComponent:a] error:nil];
+        NSDictionary *fileAttributes2 = [fileManager attributesOfItemAtPath:[directoryString stringByAppendingPathComponent:b] error:nil];
+        return [[fileAttributes1 fileCreationDate] compare:[fileAttributes2 fileCreationDate]];
+    }];
+    
+    for (NSString *filename in filenamesByCreation) {
+        [fileManager removeItemAtPath:[directoryString stringByAppendingPathComponent:filename] error:nil];
+
+        NSUInteger dirSize = [self sizeOfDirectoryURL:[self photosCacheDirURL]];
+        if (dirSize < megabytes*1024*1024) break;
+    }
+}
+
+- (UIImage *)loadImageFromCache:(NSDictionary *)imageInfo
+{
+    NSURL *filename = [[self photosCacheDirURL] URLByAppendingPathComponent:[imageInfo objectForKey:FLICKR_PHOTO_ID]];
+    NSData *imageData = [NSData dataWithContentsOfURL:filename];
+    return [UIImage imageWithData:imageData];
+}
+
+- (void)saveImageToCache:(UIImage *)image withImageInfo:(NSDictionary *)imageInfo
+{
+    [self cleanUpCacheToFitMaxSize:2];
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    NSURL *filename = [[self photosCacheDirURL] URLByAppendingPathComponent:[imageInfo objectForKey:FLICKR_PHOTO_ID]];
+    [imageData writeToURL:filename atomically:YES];
+}
+
 - (void)didReceiveMemoryWarning
 {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
     
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)setImageToImageView:(UIImage *)image
+{
+    self.scrollView.contentSize = image.size;
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+    [self.scrollView zoomToRect:self.imageView.frame animated:NO];
 }
 
 #pragma mark - View lifecycle
@@ -81,6 +161,8 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    [self setImageToImageView:[self loadImageFromCache:self.photoInfo]];
+    
     // start downloading the photo
     if (!self.imageView.image) {
         [self.activityIndicator startAnimating];
@@ -89,14 +171,11 @@
             NSURL *photoUrl = [FlickrFetcher urlForPhoto:self.photoInfo format:FlickrPhotoFormatLarge];
             NSData *photoData = [NSData dataWithContentsOfURL:photoUrl];
             UIImage *photo = [UIImage imageWithData:photoData];
+            [self saveImageToCache:photo withImageInfo:self.photoInfo];
             
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.activityIndicator stopAnimating];
-                self.scrollView.contentSize = photo.size;
-                self.imageView.image = photo;
-                [self.imageView sizeToFit];
-                [self.scrollView zoomToRect:self.imageView.frame animated:NO];
-
+                [self setImageToImageView:photo];
             });
         });
     }
