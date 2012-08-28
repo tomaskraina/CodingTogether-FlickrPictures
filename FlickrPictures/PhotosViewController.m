c53ef4976297dd08ab65dfa862e4a9da9b76d0cd
Tom K <krajeec@gmail.com>
2012-08-28 18:12:22 +0200

    show thumbnails in table view cells

--- FlickrPictures/PhotosViewController.m
+++ FlickrPictures/PhotosViewController.m
@@ -10,12 +10,15 @@
 #import "FlickrFetcher.h"
 #import "SinglePhotoViewController.h"
 #import "DetailPlaceMapViewController.h"
+#import "FileCache.h"
 
 @interface PhotosViewController() <UITableViewDelegate, UITableViewDataSource>
+@property (strong, nonatomic) FileCache *cache;
 @end
 
 @implementation PhotosViewController
 @synthesize photos = _photos;
+@synthesize cache = _cache;
 
 - (void)didReceiveMemoryWarning
 {
@@ -23,6 +26,16 @@
     [super didReceiveMemoryWarning];
     
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (FileCache *)cache
+{
+    if (!_cache) {
+        _cache = [[FileCache alloc] init];
+        _cache.maxSize = 10;
+        _cache.domain = @"thumbnails";
+    }
+    return  _cache;
 }
 
 - (void)setPhotos:(NSArray *)photos
@@ -58,6 +71,23 @@
     return self.photos.count;
 }
 
+#define ACTIVITY_INDICATOR_VIEW_TAG 1024
+
+- (void)addActivityIndicatorToView:(UIView *)view
+{
+    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityIndicator.tag = ACTIVITY_INDICATOR_VIEW_TAG;
+    activityIndicator.contentMode = UIViewContentModeCenter;
+    activityIndicator.frame = view.bounds;
+    [activityIndicator startAnimating];
+    [view addSubview:activityIndicator];
+}
+
+- (void)removeActivityIndicatorFromView:(UIView *)view
+{
+    [[view viewWithTag:ACTIVITY_INDICATOR_VIEW_TAG] removeFromSuperview];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Photo Table Cell";
@@ -80,7 +110,36 @@
         cell.textLabel.text = NSLocalizedString(@"Unknown Title", @"Photo Table Cell Unknown Title");
     }
     
+    // Activity indicator & image
+    cell.imageView.image = [UIImage imageNamed:@"placeholder"];
+    [self addActivityIndicatorToView:cell.imageView];
+
+    dispatch_queue_t loadThumbnailQueue = dispatch_queue_create("thumbnail loader", NULL);
+    dispatch_async(loadThumbnailQueue, ^{
+        NSData *photoData = [self.cache dataForKey:[photoInfo objectForKey:FLICKR_PHOTO_ID]];
+        
+        if (!photoData) {
+            NSURL *photoURL = [FlickrFetcher urlForPhoto:photoInfo format:FlickrPhotoFormatSquare];
+            photoData = [NSData dataWithContentsOfURL:photoURL];
+            [self.cache saveData:photoData forKey:[photoInfo objectForKey:FLICKR_PHOTO_ID]];
+        }
+        
+        UIImage *image = [UIImage imageWithData:photoData];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self removeActivityIndicatorFromView:cell.imageView];
+            [cell.imageView setImage:image];
+        });
+    });
+    
     return cell;
+}
+
+
+- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view
+{
+    
+
 }
 
 #pragma mark - UIStoryboardSegue
